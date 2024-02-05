ed819a1a03d1f382d92a94e5200f0ea15f5d915f
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-02-04 22:55:57 -0300

    feat: Configure admin entrypoint to redirect to services page

--- apip/admin/sites.py
+++ apip/admin/sites.py
@@ -1,6 +1,10 @@
 from django.contrib.admin.sites import AdminSite as DjangoAdminSite
+from django.shortcuts import redirect
 
 
 class AdminSite(DjangoAdminSite):
     site_header = "APIPhantom"
     site_title = "APIPhantom"
+
+    def index(self, request, extra_context=None):
+        return redirect("adminservices/service/")
