c9b25789dd38aab5f8ff73bfca6ea3686f3e7121
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-02-04 22:54:27 -0300

    feat: Adds middleware that uses the mocker to generate data from a json schema

---
+++ apip/middlewares/subdomain_mock.py
@@ -0,0 +1,49 @@
+from django.http import Http404
+from rest_framework.request import Request
+from rest_framework.response import Response
+from django.http import JsonResponse
+
+
+from apip.testplans.models import Service, TestPlan
+from apip.fackers import get_faker
+
+
+def get_request_subdomain(request: Request) -> str:
+    host: str = request.get_host()
+    host_parts = request.get_host().split(".")
+
+    if host.startswith("127.0.0.1"):
+        return None
+
+    if len(host_parts) > 1:
+        return host_parts[0]
+
+    return None
+
+
+class SubdomainMockMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: Request) -> Response:
+        subdomain = get_request_subdomain(request)
+
+        if subdomain is None:
+            return self.get_response(request)
+
+        path = request.path
+
+        if path == "/" or path == "/favicon.ico":
+            raise Http404("Subdomínio não encontrado na URL")
+
+        try:
+            service = Service.objects.get(name__icontains=subdomain)
+            test_plan = service.plans.get(endpoint=path)
+            fake_data = get_faker().generate_data_by_schema(test_plan.schema)
+            return JsonResponse(fake_data, status=200)
+
+        except Service.DoesNotExist:
+            raise Http404("Serviço não encontrado para o subdomínio")
+
+        except TestPlan.DoesNotExist:
+            raise Http404("TestPlan não encontrado para o subdomínio")
--- apip/settings.py
+++ apip/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "apip.middlewares.subdomain_mock.SubdomainMockMiddleware",
 ]
 
 ROOT_URLCONF = "apip.urls"
