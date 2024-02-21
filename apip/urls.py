f53b7e72241ddc6e4fef23c9675b61855da2c614
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-02-21 09:28:17 -0300

    feat: Redirects the index to the admin panel

--- apip/urls.py
+++ apip/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,6 +8,7 @@ from apip.testplans import urls as testplans_urls
 
 
 urlpatterns = [
+    path("", lambda: redirect("adminservices/service/", permanent=True)),
     path("admin", admin.site.urls),
     path("api/", include(testplans_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
