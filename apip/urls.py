5ba2742d6dfdfa855aac83e13380171e0beebcf0
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-02-26 09:24:59 -0300

    fix: Adjust redirection url for admin panel

--- apip/urls.py
+++ apip/urls.py
@@ -8,7 +8,7 @@ from apip.testplans import urls as testplans_urls
 
 
 urlpatterns = [
-    path("", lambda: redirect("adminservices/service/", permanent=True)),
+    path("", lambda service: redirect("adminservices/service/", permanent=True)),
     path("admin", admin.site.urls),
     path("api/", include(testplans_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
