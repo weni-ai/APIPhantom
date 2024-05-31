2b7adc3d36324beb908cfabe6362f548235f594e
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-05-31 10:36:08 -0300

    feat: Add exact_value field in the TestPlan model

--- apip/testplans/models.py
+++ apip/testplans/models.py
@@ -6,6 +6,11 @@ from apip.services.models import Service
 class TestPlan(models.Model):
     endpoint = models.CharField(max_length=255)
     service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name="plans")
+    exact_value = models.BooleanField(
+        "Exact Value",
+        help_text="This field indicates that the value returned will be the same as that defined in schema",
+        default=False
+    )
     schema = models.JSONField(null=True, default=dict)
 
     def __str__(self) -> str:
