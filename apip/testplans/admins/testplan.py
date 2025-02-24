7f7e836b5eb240235a808dcd945ff8c8335cc5b4
sandro.meireles <sandro.meirelesdev@gmail.com>
2025-02-24 20:46:21 -0300

    feat: Add redirect button to TestPlan admin interface

--- apip/testplans/admins/testplan.py
+++ apip/testplans/admins/testplan.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
-
 from apip.testcases.admins.testcase import TestCaseInline
 from apip.admin.widgets import get_ace_widget
 from ..models import TestPlan
@@ -20,6 +19,7 @@ class TestPlanForm(forms.ModelForm):
 class TestPlanAdmin(admin.ModelAdmin):
     list_display = (
         "endpoint",
+        "redirect",
         "service",
         #"test_passing",
         #"run_tests",
@@ -32,6 +32,25 @@ class TestPlanAdmin(admin.ModelAdmin):
     def run_tests(self, endpoint):
         button_html = '<a class="button" href="">Rodar testes</a>'
         return format_html(button_html)
+    
+    def redirect(self, obj: TestPlan, request):
+        protocol = 'https' if request.is_secure() else 'http'
+        request_domain = request.get_host()
+
+        url = f"{protocol}://{obj.service.subdomain}.{request_domain}{obj.endpoint}"
+        
+        html = f"""<a target="_blank" class="button" href="{url}" style="
+                padding: 4px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: fit-content;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+            </svg>
+        </a>"""
+        return format_html(html)
 
     def test_passing(self, endpoint: TestPlan):
         passing = True
@@ -47,5 +66,8 @@ class TestPlanAdmin(admin.ModelAdmin):
                 html = '<a class="button" href="">Passing</a>'
 
         return format_html(html)
+
+    def get_list_display(self, request):
+        return [field if field != "redirect" else lambda obj: self.redirect(obj, request) for field in super().get_list_display(request)]
 
     run_tests.short_description = "Rodar testes"
